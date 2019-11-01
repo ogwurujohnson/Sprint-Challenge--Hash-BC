8fbfd1dda4598bb69af66b3f49234826b8fe1d83
ogwurujohnson <ogwurujohnson@gmail.com>
2019-11-01 11:56:31 +0100

    implement the hashtable 2 problem with passing test

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,24 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    available_ticket = None
+
+    for i in range(length):
+        departure = tickets[i].source
+        arrival = tickets[i].destination
+
+        hash_table_insert(hashtable, departure, arrival)
+
+        # check if the ticket for your first flight has a destination with a source of NONE
+        if departure == "NONE":
+            available_ticket = arrival
+            # set as starting location
+            route[0] = available_ticket
+    
+    for i in range(1, length):
+        next_destination = hash_table_retrieve(hashtable, available_ticket)
+        route[i] = next_destination
+
+        available_ticket = next_destination
 
     return route
