82e43f85fda9a21c8879d0c9227b2dc63a8ef734
ogwurujohnson <ogwurujohnson@gmail.com>
2019-11-01 11:56:24 +0100

    implement the hashtable 1 problem with passing test

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,9 +12,29 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    index = 0
 
-    return None
+    for item in weights:
+        sum_weights = limit - item
+        existing_key = hash_table_retrieve(ht, item)
 
+        if (existing_key == 0 or existing_key) and (limit - item == sum_weights):
+            return (index, existing_key)
+        hash_table_insert(ht, item, index)
+        index += 1
+
+    
+    for item in weights:
+        sum_weights = limit - item
+
+        weight_index = hash_table_retrieve(ht, sum_weights)
+
+        if weight_index is not None:
+            current_index = hash_table_retrieve(ht, item)
+            if current_index > weight_index:
+                return (current_index, weight_index)
+            else:
+                return(weight_index, current_index)
 
 def print_answer(answer):
     if answer is not None:
